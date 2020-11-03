d0845189f3be46d986e5129b70476ca42faf491f
Chen Xu <xuchen@thoughtworks.com>
2020-11-03 11:28:10 +0800

    feat: print no promotion receipt

--- TakeOutFood/App.cs
+++ TakeOutFood/App.cs
@@ -1,4 +1,7 @@
-﻿namespace TakeOutFood
+﻿using System.Linq;
+using System.Text;
+
+namespace TakeOutFood
 {
     using System;
     using System.Collections.Generic;
@@ -18,7 +21,49 @@
         {
             //TODO: write code here
 
-            return null;
+            var orderItems = inputs.Select(input =>
+            {
+                var splited = input.Split('x');
+                var id = splited[0];
+                var quantity = Convert.ToInt32(splited[1]);
+                var matchedItem = this.itemRepository.FindAll().First(i => i.Id == id.Trim());
+                var name = matchedItem.Name;
+                var pricce = matchedItem.Price;
+
+                return new OrderItem(id, quantity, name, pricce);
+            }).ToList();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("============= Order details =============\n");
+
+
+            orderItems.ForEach(oi =>
+            {
+                sb.Append(string.Format($"{oi.Name} x {oi.Quantity} = {oi.Quantity * oi.Price} yuan\n"));
+            });
+            sb.Append("-----------------------------------\n");
+
+            var total = orderItems.Sum(oi => oi.Quantity * oi.Price);
+
+            sb.Append(string.Format($"Total：{total} yuan\n"));
+            sb.Append("===================================");
+            return sb.ToString();
+        }
+    }
+
+    public class OrderItem
+    {
+        public string Id { get; private set; }
+        public int Quantity { get; private set; }
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public OrderItem(string id, int quantity, string name, double price)
+        {
+            this.Id = id;
+            this.Quantity = quantity;
+            this.Name = name;
+            this.Price = price;
         }
     }
 }
