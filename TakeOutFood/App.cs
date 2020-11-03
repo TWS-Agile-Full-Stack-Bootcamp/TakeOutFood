4a6386969bf8997216e17f556170b8c6c85d0907
Zhen She <neal.seah@gmail.com>
2020-11-03 12:35:21 +0800

    feat: can calculate best charge without promotion

--- TakeOutFood/App.cs
+++ TakeOutFood/App.cs
@@ -1,24 +1,57 @@
 ﻿namespace TakeOutFood
 {
-    using System;
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
 
-    public class App
-    {
-        private IItemRepository itemRepository;
-        private ISalesPromotionRepository salesPromotionRepository;
+	public class App
+	{
+		private IItemRepository itemRepository;
+		private ISalesPromotionRepository salesPromotionRepository;
 
-        public App(IItemRepository itemRepository, ISalesPromotionRepository salesPromotionRepository)
-        {
-            this.itemRepository = itemRepository;
-            this.salesPromotionRepository = salesPromotionRepository;
-        }
+		public App(IItemRepository itemRepository, ISalesPromotionRepository salesPromotionRepository)
+		{
+			this.itemRepository = itemRepository;
+			this.salesPromotionRepository = salesPromotionRepository;
+		}
 
-        public string BestCharge(List<string> inputs)
-        {
-            //TODO: write code here
+		public string BestCharge(List<string> inputs)
+		{
+			IEnumerable<OrderItem> orderItems = this.DecodeInputs(inputs);
+			return "============= Order details =============\n" +
+					this.RenderItems(orderItems) +
+					"\n-----------------------------------\n" +
+					$"Total：{this.CalculateTotal(orderItems)} yuan\n" +
+					"===================================";
+		}
 
-            return null;
-        }
-    }
+		private double CalculateTotal(IEnumerable<OrderItem> orderItems)
+		{
+			return orderItems.Select(_ => _.Subtotal).Sum();
+		}
+
+		private IEnumerable<OrderItem> DecodeInputs(IEnumerable<string> inputs)
+		{
+			return inputs.Select(input =>
+			{
+				var splitted = input.Split(" x ");
+				var id = splitted[0];
+				int quantity = Int32.Parse(splitted[1]);
+				var item = this.itemRepository.FindAll().Find(item => item.Id == id);
+				return new OrderItem(item, quantity);
+			});
+		}
+
+		private string RenderItems(IEnumerable<OrderItem> orderItems)
+		{
+			var renderedItems = orderItems.Select(orderItem =>
+			{
+				var name = orderItem.Name;
+				var quantity = orderItem.Quantity;
+				var subtotal = orderItem.Subtotal;
+				return $"{name} x {quantity} = {subtotal} yuan";
+			});
+			return string.Join("\n", renderedItems);
+		}
+	}
 }
---
+++ TakeOutFood/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace TakeOutFood
+{
+	internal class OrderItem : Item
+	{
+		public OrderItem(Item item, int quantity) : base(item.Id, item.Name, item.Price)
+		{
+			this.Quantity = quantity;
+		}
+
+		public int Quantity { get; private set; }
+		public double Subtotal
+		{
+			get
+			{
+				return Price * Quantity;
+			}
+		}
+	}
+}
